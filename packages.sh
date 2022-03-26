779f28915b7bcd7ab5afc71a158177b087fcc411
Antoine Audrain <contact@antoineaudrain.com>
2022-03-26 16:15:03 +0100

    Update packages.sh

--- packages.sh
+++ packages.sh
@@ -15,5 +15,7 @@
 #curl https://get.docker.com | sudo sh
 #sudo usermod -aG docker $USER
 #newgrp docker
+#sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+#sudo chmod +x /usr/local/bin/docker-compose
 #
 #curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
