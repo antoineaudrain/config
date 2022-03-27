a995533f6ca4108d88a6d70a0836551390ae7986
Antoine Audrain <dev@antoineaudrain.com>
2022-03-27 12:08:47 +0200

    copy config file in shell script

--- packages.sh
+++ packages.sh
@@ -13,7 +13,7 @@ echo "deb [signed-by=/usr/share/keyrings/yarnkey.gpg] https://dl.yarnpkg.com/deb
 sudo apt-get update && sudo apt-get install yarn
 
 curl https://get.docker.com | sudo sh
-sudo usermod -aG docker $USER
+sudo usermod -aG docker "$USER"
 newgrp docker
 sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
--- shell.sh
+++ shell.sh
@@ -4,4 +4,7 @@
 sudo apt install -y zsh
 chsh -s $(which zsh)
 sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
 sudo apt-get install -y fonts-powerline
+
+cp ./zsh/zshrc ~/.zshrc
